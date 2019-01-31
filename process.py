22527ed5502a07d1ff6dc650ffe221530ee7153a
fengbo <fengbo03@126.com>
2019-01-31 16:22:01 +0700

    adding getter methods

--- process.py
+++ process.py
@@ -13,10 +13,22 @@ class Process():
     def setPriority(self, nums):
         self.priority = nums
 
+    def getName(self):
+        return self.name
+
+    def getPriority(self):
+        return self.priority
+    
+    def getArriveTime(self):
+        return self.getArriveTime
+
+    def getBurstTime(self):
+        return self.burstTime
+ 
+
     def __str__(self):
         stringBuilder = "Process " + self.name + " " + str(self.burstTime)
         stringBuilder += (" " + str(self.arriveTime) + " " + str(self.priority))
         return stringBuilder
 
-    def getBurstTime(self):
-        return self.burstTime
+   
