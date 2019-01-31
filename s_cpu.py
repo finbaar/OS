61a498d845fac97914033b39e4fb3e50370bbeb6
fengbo <fengbo03@126.com>
2019-01-31 14:16:45 +0700

    finishing collecting input and set up the ready pool

--- s_cpu.py
+++ s_cpu.py
@@ -1,17 +1,71 @@
-from random import randint
+from random import randint, sample
 
 
-numberOfProcesses = int(input("Please enter how many processes"))
+class Process():
+    """A class represents a process, including attributes like id, burst time,
+       arrive time and priority if provided"""
 
-isRandom = input("Enter y/Y to randomly generates those processes\nor any keys to type it yourself")
-print("if you intend to use priority, spcify the priority like A,25,2")
-print("p1 is the process's name and 25 is the cpu burst time, 2 means the priority")
-print("if you do not want to use priority,just omit the 2 like A,25")
+    def __init__(self, name, burstTime, arriveTime, priority=0):
+        self.name = name
+        self.burstTime = burstTime
+        self.priority = priority
+        self.arriveTime = arriveTime
+
+    def setPriority(self, nums):
+        self.priority = nums
+
+    def __str__(self):
+        stringBuilder = "Process " + self.name + " " + str(self.burstTime)
+        stringBuilder += (" " + str(self.arriveTime) + " " + str(self.priority))
+        return stringBuilder
+
+
+
+def generateProcesses(nums, needPrioiry):
+    """randomly generate n processes without priority,
+       if priority is needed, the priority would be given randomly"""
+
+    processList = []
+
+    for i in range(nums):
+        processList.append(Process(str(chr(65+i)), randint(0,20), i, 0))
+    
+    if needPrioiry:
+        #randomly chose number from the set
+        temp = sample(range(nums), nums)
+        #set each process's priority accordingly
+        for i in range(nums):
+            processList[i].setPriority(temp[i])
+    
+    return processList
+
+
+
+prompt = "1.FIFO\n2.Preemtive SJF\n3.Nonpreemtive SJF\n4.preemtive Priority\n5.Nonpreemtive priority\n6.Round Robin\n"
+choiceOfAlgorithm = int(input(prompt))
+numberOfProcesses = int(input("Please enter how many processes\n"))
+isRandom = input("Enter y/Y to reandomly generat processes or other keys to continue\n")
 
 theReadyQueue = []
 
-if isRandom == 'y' or isRandom == 'Y':
-    #randomly generated
-    pass
+if choiceOfAlgorithm == 4 or choiceOfAlgorithm == 5:
+    requirePriority = True
 else:
-    pass
+    requirePriority = False
+
+if isRandom == 'y' or isRandom == 'Y':
+    #randomly generate the processes and put them into the ready queue
+    theReadyQueue = generateProcesses(numberOfProcesses,requirePriority)
+else:
+    for i in range(numberOfProcesses):
+        ProcessId = str(chr(65+i))
+        print("Processes " + ProcessId + ": (burst time, arrive time, priority(if required) )")
+        temp = input().split(",")
+        if len(temp) == 2:
+            theReadyQueue.append(Process(ProcessId, int(temp[0]), int(temp[1])))
+        else:
+            theReadyQueue.append(Process(ProcessId, int(temp[0]), int(temp[1]), int(temp[2])))
+
+
+for item in theReadyQueue:
+    print(item)
